2b2be435f30d2a98288d44f486a52e3dcdcfefc1
rahuljames01 <rahulkriplani41@gmail.com>
2024-02-12 12:37:19 +0530

    updated

--- juhoshi/src/App.jsx
+++ juhoshi/src/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import Axios from "axios";
 import "./App.css";
 import SignUp from "./pages/SignUp";
